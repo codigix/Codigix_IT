756e193d07ac4cb2dd646b56fe3f61b58e90ecb9
RuchitaSonar <118338252+RuchitaSonar@users.noreply.github.com>
2026-02-11 13:47:31 +0530

    changes

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,12 +1,23 @@
-require('dotenv').config();
+// require('dotenv').config();
+// const path = require('path');
+
+// module.exports = {
+//   PORT: process.env.PORT || 5000,
+//   DB_HOST: process.env.DB_HOST || 'localhost',
+//   DB_USER: process.env.DB_USER || 'root',
+//   DB_PASSWORD: process.env.DB_PASSWORD || 'Ruchita@12345',
+//   DB_NAME: process.env.DB_NAME || 'codigix_admin',
+//   JWT_SECRET: process.env.JWT_SECRET || 'codigix_admin_secret_key_2026',
+//   DATA_FILE: path.join(__dirname, '../data/data.json')
+// };
 const path = require('path');
 
 module.exports = {
-  PORT: process.env.PORT || 5000,
-  DB_HOST: process.env.DB_HOST || 'localhost',
-  DB_USER: process.env.DB_USER || 'root',
-  DB_PASSWORD: process.env.DB_PASSWORD || '',
-  DB_NAME: process.env.DB_NAME || 'codigix_admin',
-  JWT_SECRET: process.env.JWT_SECRET || 'codigix_admin_secret_key_2026',
+  PORT: 5000,
+  DB_USER: 'appuser',
+  DB_PASSWORD: 'Appuser@123',
+  DB_NAME: 'codigix_admin',
+  DB_HOST: 'localhost',
+  JWT_SECRET: 'codigix_admin_secret_key_2026',
   DATA_FILE: path.join(__dirname, '../data/data.json')
 };
--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,31 @@
-const mysql = require('mysql2/promise');
-const config = require('./config');
+// const mysql = require('mysql2/promise');
+// const config = require('./config');
 
+// const pool = mysql.createPool({
+//   host: config.DB_HOST,
+//   user: config.DB_USER,
+//   password: config.DB_PASSWORD,
+//   database: config.DB_NAME,
+//    port: 3307, 
+//   waitForConnections: true,
+//   connectionLimit: 10,
+   
+//   queueLimit: 0
+// });
+// // module.exports = db;
+// module.exports = pool;
+const mysql = require('mysql2/promise');
+console.log("Connecting DB HOST = 127.0.0.1");
 const pool = mysql.createPool({
-  host: config.DB_HOST,
-  user: config.DB_USER,
-  password: config.DB_PASSWORD,
-  database: config.DB_NAME,
+  host: '127.0.0.1',
+  user: 'appuser',
+  password: 'Appuser@123',
+  database: 'codigix_admin',
+  port: 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
 });
+
 
 module.exports = pool;
