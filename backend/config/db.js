4d73110a767351508f684b851ceb05c310db922a
backendcodigix <backendcodigix@gmail.com>
2026-02-11 14:59:24 +0530

    push

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,31 +1,17 @@
-// const mysql = require('mysql2/promise');
-// const config = require('./config');
-
-// const pool = mysql.createPool({
-//   host: config.DB_HOST,
-//   user: config.DB_USER,
-//   password: config.DB_PASSWORD,
-//   database: config.DB_NAME,
-//    port: 3307, 
-//   waitForConnections: true,
-//   connectionLimit: 10,
-   
-//   queueLimit: 0
-// });
-// // module.exports = db;
-// module.exports = pool;
 const mysql = require('mysql2/promise');
-console.log("Connecting DB HOST = 127.0.0.1");
+const config = require('./config');
+
+console.log(`Connecting to DB: ${config.DB_HOST}:${config.DB_PORT}`);
+
 const pool = mysql.createPool({
-  host: '127.0.0.1',
-  user: 'appuser',
-  password: 'Appuser@123',
-  database: 'codigix_admin',
-  port: 3306,
+  host: config.DB_HOST,
+  user: config.DB_USER,
+  password: config.DB_PASSWORD,
+  database: config.DB_NAME,
+  port: parseInt(config.DB_PORT),
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
 });
-
 
 module.exports = pool;
